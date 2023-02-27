0fca56aed0a85155aead291834b652049a102752
Rohan Dhangar <81848857+RohanDhangar@users.noreply.github.com>
2023-02-27 11:42:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./App.css";
+// import "./App.css";
 import AllTodos from "./Components/AllTodos";
 import TodoForm from "./Components/TodoForm";
 const App = () => {
